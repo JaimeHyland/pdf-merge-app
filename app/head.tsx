8b9a16fbe1d452af77a17b7aa041aab267e916ff
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-07-23 14:25:23 +0200

    Correct text on Save button in app/page.tsx 2nd try.

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Merge PDFs Securely</title>
+      <meta name="description" content="Client-side PDF merger – files never leave your device" />
+    </>
+  )
+}
